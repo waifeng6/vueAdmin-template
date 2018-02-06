914f23dc741ac254a61ffd262b141e80b73ed86d
zhanglf <251190220@qq.com>
2018-02-07 04:53:40 +0800

    第一次提交一个没有错误的本地库

--- config/index.js
+++ config/index.js
@@ -6,11 +6,11 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
